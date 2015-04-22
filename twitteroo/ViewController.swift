240a9806bec82b934b8dae807043bac87d570ed6
Sherman Leung <shermankleung@gmail.com>
2015-04-21 19:51:43 -0700

    Initial Commit

---
+++ twitteroo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitteroo
+//
+//  Created by Sherman Leung on 4/21/15.
+//  Copyright (c) 2015 Sherman Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
